e8488f19275d1686daaa29c26e779c0b8a4c2d36
Arhanys <ethan.colinet.pro@gmail.com>
2025-12-15 00:07:24 +0100

    tailwind custom and first routing check

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <div className="text-xl font-barlow-condensed">Hello World</div>
+    </>
+  );
+}
---
+++ src/pages/shop/index.tsx
@@ -0,0 +1,7 @@
+export default function Shop() {
+  return (
+    <>
+      <div className="text-xl font-barlow-condensed">Shop is working</div>
+    </>
+  );
+}
